f65185e63286ec290e38389f93ec8756892035e6
maGC <maGC@host.docker.internal>
2022-03-16 19:21:44 +0100

    Version 2

--- EDTarea3/src/UD4.java
+++ EDTarea3/src/UD4.java
@@ -3,6 +3,6 @@ public class UD4 {
 
 	public static void main(String[] args) {
 		
-		System.out.print("En este proyecto trabajamos con el repositorio GITHUB");
+		System.out.println("En este proyecto trabajamos con el repositorio GITHUB");
 	}
 }
